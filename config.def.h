22cc21e2b903ed0a14edf03b17106cfad7703a8b
Francesco Saccone <francesco@francescosaccone.com>
2026-01-03 10:54:27 +0100

    config.def.h: use correct font name

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "OfficeCodePro:pixelsize=26:antialias=true:autohint=true";
+static char *font = "Office Code Pro:pixelsize=26:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
